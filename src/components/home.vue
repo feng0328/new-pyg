b01f087d7a1082ee056bbf9b8b17d966d5f15dd9
ljf <296348573@qq.com>
2019-02-16 20:24:25 +0800

    home-顶部导航设置完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,26 +1,157 @@
 <template>
   <el-container class="container">
-    <el-header>Header</el-header>
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">
+            <img src="../assets/logo.png" alt>
+          </div>
+        </el-col>
+        <el-col :span="19" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href="#" class="loginout" @click.prevent="handleLoginout()">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-aside class="aside" width="200px">
+        <!-- 侧边栏 -->
+        <el-menu default-active="2" unique-opened :router="true">
+          <!-- 导航一 用户 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              用户列表
+            </el-menu-item>
+          </el-submenu>
+          <!-- 导航2权限 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-edit-outline"></i>
+              角色列表
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-edit-outline"></i>
+              权限列表
+            </el-menu-item>
+          </el-submenu>
+          <!-- 导航三 商品 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-goods"></i>
+              商品列表
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-goods"></i>
+              分类参数
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-goods"></i>
+              商品分类
+            </el-menu-item>
+          </el-submenu>
+          <!-- 导航四 订单 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-service"></i>
+              订单列表
+            </el-menu-item>
+          </el-submenu>
+          <!-- 导航五 报表 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-document"></i>
+              数据报表
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {}
+export default {
+  beforeMount() {
+    if(!localStorage.getItem('token')){
+      this.$router.push({
+        name:'login'
+      })
+    }
+  },
+  methods: {
+    handleLoginout(){
+      localStorage.clear();
+      this.$router.push({
+        name:'login'
+      });
+      this.$message.warning('退出成功')
+    }
+
+  },
+
+};
 </script>
 
 <style>
 .container {
-    background-color: yellow;
-    height: 100%;
+  background-color: yellow;
+  height: 100%;
 }
 .aside {
-    background-color: red;
+  background-color: red;
 }
 .main {
-    background-color: blue;
+  background-color: blue;
+}
+.header {
+  background-color: #b3c0d1;
+  padding: 0;
+}
+
+.header .middle {
+  line-height: 60px;
+  color: #fff;
+  text-align: center;
+}
+
+.header .loginout {
+  line-height: 60px;
+  text-decoration: none;
+}
+
+.aside {
+  background-color: #d3dce6;
+}
+.aside .menu {
+  height: 100%;
+}
+
+.main {
+  background-color: #e9eef3;
+  height: 100%;
 }
 </style>
