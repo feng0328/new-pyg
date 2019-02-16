6c61c5377efa0e72262c6ed8a9ae3684d331fd66
ljf <296348573@qq.com>
2019-02-16 15:37:14 +0800

    昨日项目上传

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,15 +1,26 @@
 <template>
-    <div>
-        home ----
-    </div>
+  <el-container class="container">
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
+.container {
+    background-color: yellow;
+    height: 100%;
+}
+.aside {
+    background-color: red;
+}
+.main {
+    background-color: blue;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,19 +3,18 @@ import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/login.vue'
 import Home from '@/components/home.vue'
+import '@/assets/css/baes.css'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },{
-      path: '/',
-      name: 'home',
-      component: Home
-    }
-  ]
+  routes: [{
+    path: '/login',
+    name: 'login',
+    component: Login
+  }, {
+    path: '/',
+    name: 'home',
+    component: Home
+  }]
 })
